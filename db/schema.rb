0de1127c4b28731eefdf87274b6d7b42862b53af
Aryan pal <aryan.pal0ppq@gmail.com>
2023-01-10 14:27:06 +0530

    updated database url

--- db/schema.rb
+++ db/schema.rb
@@ -25,10 +25,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_05_144407) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "homes", force: :cascade do |t|
-    t.string "index"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
